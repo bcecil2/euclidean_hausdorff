8f2f99661734909589ddac27ab34fcbb4322231b
vlad-oles <vlad.oles@protonmail.com>
2024-02-12 08:03:18 -0800

    class Points handles dimensionality of rotation angle

--- points.py
+++ points.py
@@ -1,27 +1,24 @@
-import numpy as np
-
-from transformation import Transformation
 from geometry import rot2d, refl2d
 
 
 class Points(object):
 
-    def __init__(self, points):
-        self.n, self.n_dim = points.shape
-        self.coords = points
+    def __init__(self, coords):
+        self.n, self.k = coords.shape
+        self.coords = coords
 
-    def reflect(self, is_reflection):
-        assert self.n_dim == 2, 'reflection in 3D not implemented'
+    def reflect(self, reflection):
+        assert self.k == 2, 'reflection is only implemented in 2D'
 
-        return Points(self.coords @ refl2d(is_reflection).T)
+        return Points(self.coords @ refl2d(reflection).T)
 
     def rotate(self, theta):
-        assert self.n_dim == 2, 'rotation in 3D not implemented'
+        assert self.k == 2, 'rotation is only implemented in 2D'
 
-        return Points(self.coords @ rot2d(theta).T)
+        return Points(self.coords @ rot2d(theta[0]).T)
 
     def shift(self, delta):
         return Points(self.coords + delta)
 
     def transform(self, T):
-        return self.reflect(T.is_reflection).rotate(T.angle).shift(T.shift)
+        return self.reflect(T.reflection).rotate(T.theta).shift(T.delta)
