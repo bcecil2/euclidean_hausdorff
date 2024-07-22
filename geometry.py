fb5477fec9c6855d052fedacc254e3fbc551f6d3
bcecil2 <blakemadness@gmail.com>
2024-07-22 14:40:06 -0700

    removed old rotation and reflection matrix code

--- geometry.py
+++ geometry.py
@@ -1,26 +1,6 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-
-def rot2d(theta,invert=False):
-      if invert:
-          theta = -theta
-      return np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
-
-def rot3d(n,theta,invert=False):
-    #page 5 from here https://scipp.ucsc.edu/~haber/ph216/rotation_12.pdf
-    n1,n2,n3 = n
-    c,s = np.cos(theta),np.sin(theta)
-    row1 = [c + n1**2*(1-c), n1*n2*(1-c) - n3*s, n1*n3*(1-c) + n2*s]
-    row2 = [n1*n2*(1-c) + n3*s, c + n2**2*(1-c), n2*n3*(1-c)-n1*s]
-    row3 = [n1*n3*(1-c)-n2*s, n2*n3*(1-c) + n1*s, c + n3**2*(1-c)]
-    R = np.array([row1,row2,row3])
-    if invert:
-        return np.linalg.inv(R)
-    else:
-        return R
-
-
 def make_rot_mx(k, rho):
     """
     Compile rotation matrix for a given angle.
