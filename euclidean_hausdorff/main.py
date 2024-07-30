f4994f7fe54049b8f3590e15837fb3b809c3882e
vlad-oles <vlad.oles@protonmail.com>
2024-07-29 22:06:49 -0700

    fixed imports and added package description stub

--- euclidean_hausdorff/main.py
+++ euclidean_hausdorff/main.py
@@ -1,10 +1,9 @@
+import numpy as np
 from euclidean_hausdorff.eucl_haus import upper_heuristic
-from euclidean_hausdorff.transformation import Transformation
-from euclidean_hausdorff.utils import *
 
 if __name__ == '__main__':
-    # examples go here
-    A = np.random.randn(100,3)
-    B = np.random.randn(200,3)
-    upper_heuristic(A,B)
+    # Examples go here:
+    A_coords = np.random.randn(100, 3)
+    B_coords = np.random.randn(200, 3)
+    upper_heuristic(A_coords, B_coords)
 
