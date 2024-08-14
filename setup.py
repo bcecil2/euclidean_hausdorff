af277a9377a6cffa5346e550f9d7285d3e98dc79
bcecil2 <blakemadness@gmail.com>
2024-08-13 22:31:41 -0700

    fixed importing error and released new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='euclidean_hausdorff',
-    version='1.0.1',
+    version='1.0.2',
     author='Vladyslav Oles',
     author_email='vlad.oles@proton.me',
     packages=['euclidean_hausdorff'],
