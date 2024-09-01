524a73dff9424a9e1ba9cc1ec53f00e1a5b2967b
vlad-oles <vlad.oles@protonmail.com>
2024-09-01 01:49:53 -0700

    corrected project description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='vlad.oles@proton.me',
     packages=['euclidean_hausdorff'],
     url='http://pypi.python.org/pypi/euclidean-hausdorff/',
-    description="quick approximation of the Gromov–Hausdorff distance using Euclidean isometries",
+    description="quick approximation of the Gromov–Hausdorff distance restricted to Euclidean isometries",
     long_description=open('README.md').read(),
     install_requires=[
         "scipy >= 1.12.0",
