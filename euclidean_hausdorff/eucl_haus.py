e1bcc18027bead9ead76d9219cd1081ebec1c99b
vlad-oles <vlad.oles@protonmail.com>
2024-09-01 01:49:21 -0700

    fixed issue with covering radii balance

--- euclidean_hausdorff/eucl_haus.py
+++ euclidean_hausdorff/eucl_haus.py
@@ -106,8 +106,8 @@ def upper(A_coords, B_coords, target_acc=None, target_err=None, max_n_no_improv=
     r = np.linalg.norm(normalized_coords, axis=1).max()
     dim_delta, dim_rho = k, k * (k - 1) // 2
     sigmas = [False] if proper_rigid else [False, True]
-    h_delta, h_rho = 4*r, 2 if dim_delta == 2 else 4    # level-0 cell sizes (s.t. ∆ has 1 point)
-    eps_delta, eps_rho = h_delta * np.sqrt(dim_delta) / 2, h_rho * np.sqrt(dim_rho) / 2 # level-0 covering radii
+    eps_delta, eps_rho = 2*r, 1 if dim_delta == 2 else 2    # level-0 covering radii (s.t. ∆ has 1 point)
+    h_delta, h_rho = 2*eps_delta/np.sqrt(dim_delta), 2*eps_rho/np.sqrt(dim_rho) # level-0 cell sizes
 
     # Initialize queue with the multiscale search grid points.
     Q = SortedList()
