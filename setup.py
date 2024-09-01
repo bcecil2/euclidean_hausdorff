5fe159dc012f2386d089bd590d2b9bb8fbce3689
vlad-oles <vlad.oles@protonmail.com>
2024-09-01 01:50:20 -0700

    bumped version to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='euclidean_hausdorff',
-    version='1.1.0',
+    version='1.1.1',
     author='Vladyslav Oles, Blake Cecil',
     author_email='vlad.oles@proton.me',
     packages=['euclidean_hausdorff'],
